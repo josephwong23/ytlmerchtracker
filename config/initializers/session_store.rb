f71b49f6884f8cf5cef1c2b3bc6c48cf9916e04b
administrator <administrator@administrators-iMac.local>
2018-05-18 17:18:13 +0800

    new edit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_fossaegean-inventory_session'
+Rails.application.config.session_store :cookie_store, key: '_YTLCC-inventory_session'
